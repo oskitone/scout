c82c5d26a73177f3a072c94b0187c623157b8526
Tommy <tommy@iamnotagoodartist.com>
2021-04-16 14:01:19 -0700

    Improve glide
    * Control step with control, like pot
    * Tighten responsiveness by ditching frequency throttling and
      optionalizing Serial usage
    * Account for minor tolerance when "off"
    * Decouple glide amount from its step derivation
    
    And some unrelated stuff:
    
    * Guess at reasonable octave and glide

--- arduino/scout/scout.ino
+++ arduino/scout/scout.ino
@@ -5,21 +5,26 @@
 #include "KeyBuffer.h"
 
 int OCTAVE_RANGE = 6;
+float GLIDE_STEP_RANGE = 2;
 
-int octaveRangePin = A0;
+int octaveControlPin = A0;
+int glideStepControlPin = A1;
 int speakerPin = 12;
+bool printToSerial = false;
 
 float notes[] = {174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25};
 
 KeyBuffer buffer;
 
-int octave;
+int octave = OCTAVE_RANGE / 2;
+float glide = .5;
 
-long octaveThrottle = 200;
-long frequencyThrottle = 24; // max
+float POT_TOLERANCE = .05;
 
-float glideStep = 10; // TODO: pot control like octave
+long settingsThrottle = 500;
+
 float frequency = 0;
+float glideStep = glide * GLIDE_STEP_RANGE;
 
 void setup() {
   Serial.begin(9600);
@@ -34,44 +39,49 @@ float getTargetFrequency() {
   return getFrequency(buffer.getFirst());
 }
 
-unsigned long frequencyPreviousMillis = 0;
 void updateFrequency() {
-  unsigned long currentMillis = millis();
+  float target = getTargetFrequency();
 
-  if (
-    (frequency == 0) ||
-    ((unsigned long)(currentMillis - frequencyPreviousMillis) >= frequencyThrottle)
-  ) {
-    float target = getTargetFrequency();
-
-    if ((frequency == 0) || (glideStep == 0)) {
-      frequency = target;
-    } else if (frequency != target) {
-      frequency = (target > frequency)
-                  ? min(target, frequency + glideStep)
-                  : max(target, frequency - glideStep);
-    }
-
-    frequencyPreviousMillis = millis();
+  if ((frequency == 0) || (glide <= POT_TOLERANCE)) {
+    frequency = target;
+  } else if (frequency != target) {
+    frequency = (target > frequency)
+                ? min(target, frequency + glideStep)
+                : max(target, frequency - glideStep);
   }
 }
 
-unsigned long octavePreviousMillis = 0;
-void updateOctave() {
+float getVoltage(int pin) {
+  return analogRead(pin) * (5.0 / 1023.0) / 5;
+}
+
+unsigned long settingsPreviousMillis = 0;
+void updateSettings() {
   unsigned long currentMillis = millis();
 
-  if ((unsigned long)(currentMillis - octavePreviousMillis) >= octaveThrottle) {
-    float voltage = analogRead(octaveRangePin) * (5.0 / 1023.0);
-    octave = round(voltage / 5 * (OCTAVE_RANGE - 1)) + 1;
+  if ((unsigned long)(currentMillis - settingsPreviousMillis) >= settingsThrottle) {
+    octave = round(getVoltage(octaveControlPin) * (OCTAVE_RANGE - 1)) + 1;
 
-    octavePreviousMillis = millis();
+    glide = getVoltage(glideStepControlPin);
+    glideStep = GLIDE_STEP_RANGE - glide * GLIDE_STEP_RANGE;
+
+    settingsPreviousMillis = millis();
   }
 }
 
 void loop() {
   buffer.populate();
 
-  updateOctave();
+  updateSettings();
+
+  if (printToSerial) {
+    Serial.println(
+      "frequency: " + String(frequency)
+      + "\ttarget: " + String(getTargetFrequency())
+      + "glideStep: " + String(glideStep)
+      + "  glide: " + String(glide)
+    );
+  }
 
   if (buffer.isEmpty()) {
     frequency = 0;
@@ -83,10 +93,5 @@ void loop() {
 
     tone(speakerPin, frequency);
     digitalWrite(LED_BUILTIN, HIGH);
-
-    Serial.println(
-      "frequency: " + String(frequency)
-      + "\ttarget: " + String(getTargetFrequency())
-    );
   }
 }
