b97d02fb5db9ab9d902eab8e46f3d912c15b259a
Tommy <tommy@iamnotagoodartist.com>
2021-07-26 18:17:17 -0700

    Stub out working burn_chip.sh script
    1. Burns bootloader using AVR programmer
    2. Uploads code using UART header
    
    IDK what exactly the fuses are and the hex paths will probably break
    when I close the Arduino IDO, but it works!

---
+++ burn_chip.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+{
+
+# Exit on error
+set -o errexit
+set -o errtrace
+
+avrdude_folder="$HOME/Library/Arduino15/packages/arduino"
+avrdude="$avrdude_folder/tools/avrdude/6.3.0-arduino17/bin/avrdude"
+avrdude_config="$avrdude_folder/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf"
+
+function burn_bootloader_using_programmer() {
+    $avrdude \
+        -C"$avrdude_config" \
+        -v \
+        -patmega328p \
+        -cusbtiny \
+        -e \
+        -Ulock:w:0x3F:m \
+        -Uefuse:w:0xFD:m \
+        -Uhfuse:w:0xDE:m \
+        -Ulfuse:w:0xFF:m
+
+    $avrdude \
+        -C"$avrdude_config" \
+        -v \
+        -patmega328p \
+        -cusbtiny \
+        -U"flash:w:$avrdude_folder/hardware/avr/1.8.3/bootloaders/optiboot/optiboot_atmega328.hex:i" \
+        -Ulock:w:0x0F:m
+}
+
+function upload_program_using_uart_with_adafruit_cable() {
+    $avrdude \
+        -C"$avrdude_config" \
+        -v \
+        -patmega328p \
+        -carduino \
+        -P/dev/cu.usbserial-AB0LJLX7 \
+        -b115200 \
+        -D \
+        -Uflash:w:/var/folders/br/87phc45d2szgdhd23089g7m40000gn/T/arduino_build_95463/scout.ino.hex:i
+}
+
+burn_bootloader_using_programmer
+upload_program_using_uart_with_adafruit_cable
+
+}
