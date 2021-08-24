cc3b95488f57fb23d945eab8d731d3aa7c061cf1
Tommy <tommy@iamnotagoodartist.com>
2021-08-24 15:37:31 -0700

    Show how to update octave using a potentiometer
    This is pulled out into an updateSettings function, which throttles
    itself to only running fully every 100ms. This prevents it from
    adversely effecting the main sound loop.

--- arduino/scout/scout.ino
+++ arduino/scout/scout.ino
@@ -8,10 +8,13 @@ float glide = .25;
 bool glideOnFreshKeyPresses = true;
 bool printToSerial = false;
 
+const int OCTAVE_RANGE = 6;
+
 const int CYCLES_PER_GLIDE_MAX = printToSerial ? 25 : 250;
 const int STARTING_NOTE_DISTANCE_FROM_MIDDLE_A = -9;
 
 const int SPEAKER_PIN = 11;
+const int OCTAVE_CONTROL_PIN = A0;
 
 Notes notes(STARTING_NOTE_DISTANCE_FROM_MIDDLE_A);
 KeyBuffer buffer;
@@ -28,15 +31,36 @@ void blink(int count = 2, int wait = 200) {
   }
 }
 
+float getVoltage(int pin) {
+  return analogRead(pin) * (5.0 / 1023.0) / 5;
+}
+
+unsigned long settingsPreviousMillis = 0;
+const long settingsThrottle = 100;
+void updateSettings(bool skipPoll = false) {
+  bool pollPasses =
+    (unsigned long)(millis() - settingsPreviousMillis) >= settingsThrottle;
+
+  if (skipPoll || pollPasses) {
+    octave = round(getVoltage(OCTAVE_CONTROL_PIN) * (OCTAVE_RANGE - 1)) + 1;
+
+    settingsPreviousMillis = millis();
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 
   blink();
+
+  updateSettings(true);
 }
 
 void loop() {
   buffer.populate();
+
+  updateSettings();
 
   if (printToSerial) {
     frequency.print();
